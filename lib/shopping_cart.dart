16e6ac682c8d07339b818f96909257d9d8085c63
unknown <michaelmarcellino54@gmail.com>
2023-12-18 20:03:20 +0700

    perbaikan cart

--- lib/shopping_cart.dart
+++ lib/shopping_cart.dart
@@ -15,110 +15,144 @@ class ShoppingCart extends StatefulWidget {
 }
 
 class _ShoppingCartState extends State<ShoppingCart> {
-Future<List<Cart>> fetchProduct(request) async {
-    var url = Uri.parse(
-        'http://libpanda-e15-tk.pbp.cs.ui.ac.id/shoppingcart/json/');
-        
-    var response = await request.get(
-        'http://libpanda-e15-tk.pbp.cs.ui.ac.id/shoppingcart/json/',
-    );
+  
+  List<Cart> list_cart = [];
 
-    List<Cart> list_cart = [];
-    for (var d in response) {
-        if (d != null) {
-            list_cart.add(Cart.fromJson(d));
-        }
-    }
-
-    void deleteAllItems() {
+  void deleteAllItems() {
+    setState(() {
       list_cart.clear();
-    }
+    });
+  }
+    
+  Future<List<Cart>> fetchProduct(request) async {
+      var url = Uri.parse(
+          'http://libpanda-e15-tk.pbp.cs.ui.ac.id/shoppingcart/json/');
+          
+      var response = await request.get(
+          'http://libpanda-e15-tk.pbp.cs.ui.ac.id/shoppingcart/json/',
+      );
 
-    return list_cart;
-}
+      for (var d in response) {
+          if (d != null) {
+              list_cart.add(Cart.fromJson(d));
+          }
+      }
+
+      return list_cart;
+  }
 
 @override
 Widget build(BuildContext context) {
   final request = context.watch<CookieRequest>();
     return Scaffold(
-        appBar: AppBar(
-        title: const Text('Shopping Cart'),
-        ),
-        //drawer: const LeftDrawer(),
-        body: FutureBuilder(
-            future: fetchProduct(request),
-            builder: (context, AsyncSnapshot snapshot) {
-                if (snapshot.data == null) {
-                    return const Center(child: CircularProgressIndicator());
-                } else {
-                    if (!snapshot.hasData) {
-                    return const Column(
-                        children: [
-                        Text(
-                            "Tidak ada data produk.",
-                            style:
-                                TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                        ),
-                        SizedBox(height: 8),
-                        ],
-                    );
-                } else {
-                    return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (_, index) => Container(
-                                margin: 
-                                  const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 12
+      appBar: AppBar(
+      title: const Text('Shopping Cart'),
+      ),
+      //drawer: const LeftDrawer(),
+      body: FutureBuilder(
+        future: fetchProduct(request),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } 
+          else {
+            if (!snapshot.hasData) {
+              return const Column(
+                children: [
+                  Text(
+                    "Tidak ada data produk.",
+                    style:
+                      TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } 
+            else {
+              double totalPrice = snapshot.data!
+                  .map((cart) => cart.book.price)
+                  .fold(0, (prev, price) => prev + price);
+              
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (_, index) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 10),
+                            Image.network(
+                              "${snapshot.data![index].book.thumbnail}",
+                              width: 100,
+                              height: 150,
+                              fit: BoxFit.cover,
+                              errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+                                return Container(
+                                  width: 100,
+                                  height: 150,
+                                  color: Colors.grey,
+                                  child: Center(
+                                    child: Icon(Icons.error),
                                   ),
-                                padding: const EdgeInsets.all(20.0),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const SizedBox(height: 10),
-                                    Image.network(
-                                      "${snapshot.data![index].book.thumbnail}",
-                                      width: 100,
-                                      height: 150,
-                                      fit: BoxFit.cover,
-                                      errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-                                        return Container(
-                                          width: 100,
-                                          height: 150,
-                                          color: Colors.grey,
-                                          child: Center(
-                                            child: Icon(Icons.error),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                    Text(
-                                    "${snapshot.data![index].book.title}",
-                                    style: const TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                    ),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text("${snapshot.data![index].book.price}"),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                        "${snapshot.data![index].book.categories}")
-                                  ],
-                                ),
-                            )
-                          );
-                    }
-                }
-              }
-            ),
-            
-          floatingActionButton: FloatingActionButton.extended (
-            onPressed: deleteAllItems,
-            label: Text('BUY'),
-            icon: Icon(Icons.shopping_cart),
-            backgroundColor: Colors.blue,
-          ),
-        );
-    }
+                                );
+                              },
+                            ),
+                            Text(
+                              "${snapshot.data![index].book.title}",
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Text("${snapshot.data![index].book.price}"),
+                            const SizedBox(height: 10),
+                            Text("${snapshot.data![index].book.categories}")
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    color: Colors.grey[200],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Total Price:",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "\$$totalPrice",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      deleteAllItems();
+                    },
+                    child: Text("Buy"),
+                  ),
+                ],
+              );
+            }
+          }
+        }
+      ),
+    );
+  }
 }
